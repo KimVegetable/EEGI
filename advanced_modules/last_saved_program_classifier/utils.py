59ac4a6ca58b1ed230c9a101645291ebaad0adc2
DESKTOP-8O7P41P\Juno <rlawnsgh92@naver.com>
2024-02-06 15:19:03 +0900

    no message

--- advanced_modules/last_saved_program_classifier/utils.py
+++ advanced_modules/last_saved_program_classifier/utils.py
@@ -1,3 +1,5 @@
-DOCX_DIR_PATH = 'advanced_modules\\last_saved_program_classifier\\Dataset\\Dataset[Docx_Total].csv'
-XLSX_DIR_PATH = "advanced_modules\\last_saved_program_classifier\\Dataset\\Dataset[Xlsx_Total].csv"
-PPTX_DIR_PATH = "advanced_modules\\last_saved_program_classifier\\Dataset\\Dataset[Pptx_Total].csv"
+import os
+
+DOCX_DIR_PATH = os.getcwd() + "advanced_modules\\last_saved_program_classifier\\Dataset\\Dataset[Docx_Total].csv"
+XLSX_DIR_PATH = os.getcwd() + "advanced_modules\\last_saved_program_classifier\\Dataset\\Dataset[Xlsx_Total].csv"
+PPTX_DIR_PATH = os.getcwd() + "advanced_modules\\last_saved_program_classifier\\Dataset\\Dataset[Pptx_Total].csv"
